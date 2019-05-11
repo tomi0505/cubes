0d424e9785056192edae69ab5d0a956dc56e2561
tomi0505 <tomaszgrzyb.online@gmail.com>
2019-05-11 16:04:24 +0200

    header over.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './scss/style.scss';
+import siemaSlider from './js/siema-slider';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sliderPrevSlideBtn = document.querySelector('.slider__prev-slide-btn');
+  const sliderNextSlideBtn = document.querySelector('.slider__next-slide-btn');
+
+  sliderPrevSlideBtn.addEventListener("click", () => siemaSlider.prev());
+  sliderNextSlideBtn.addEventListener("click", () => siemaSlider.next());
+}, false);
